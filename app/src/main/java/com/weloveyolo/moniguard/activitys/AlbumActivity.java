33346e3904214f3a48914110ce88f99ec08106e3
Your Name <email@example.com>
2024-03-27 16:30:37 +0800

    功能页布局

--- app/src/main/java/com/weloveyolo/moniguard/activitys/AlbumActivity.java
+++ app/src/main/java/com/weloveyolo/moniguard/activitys/AlbumActivity.java
@@ -1,4 +1,8 @@
 package com.weloveyolo.moniguard.activitys;
-
+import android.os.Bundle;
+import com.weloveyolo.moniguard.R;
+import com.weloveyolo.moniguard.utils.CustomToast;
 public class AlbumActivity {
-}
+    }
+
+

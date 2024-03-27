23bd0bddc2ea2732140e2e052c9a2f3f96f83a87
zr <3064510297@qq.com>
2024-03-28 01:09:09 +0800

    主页到新建场景的跳转

--- app/src/main/java/com/weloveyolo/moniguard/activitys/AddSceneActivity.java
+++ app/src/main/java/com/weloveyolo/moniguard/activitys/AddSceneActivity.java
@@ -1,20 +1,22 @@
 package com.weloveyolo.moniguard.activitys;
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.weloveyolo.moniguard.R;
-public class HomeActivity extends AppCompatActivity {
+
+public class AddSceneActivity extends AppCompatActivity {
+
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_home);
-
+        setContentView(R.layout.add_scene);
     }
-
 }

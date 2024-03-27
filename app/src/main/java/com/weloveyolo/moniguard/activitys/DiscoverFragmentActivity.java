cb5a8ea95badfcb07d84f9b50c2d7a9a509dcc58
ChenZhongWei <2230776247@qq.com>
2024-03-27 23:52:31 +0800

    改bug

--- app/src/main/java/com/weloveyolo/moniguard/activitys/DiscoverFragmentActivity.java
+++ app/src/main/java/com/weloveyolo/moniguard/activitys/DiscoverFragmentActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.weloveyolo.moniguard.MainActivity;
 import com.weloveyolo.moniguard.R;
 
 public class DiscoverFragmentActivity extends AppCompatActivity {
@@ -20,16 +21,7 @@ public class DiscoverFragmentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_discover);
 
-        Button button1 = findViewById(R.id.smart_photo_album);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(DiscoverFragmentActivity.this, AlbumActivity.class);
-                Toast.makeText(DiscoverFragmentActivity.this, "You clicked", Toast.LENGTH_SHORT).show();
-                String data="123";
-                Log.d("SecondActivity",data);
-                startActivity(intent);
-            }
-        });
+
+
     }
 }
--- app/src/main/java/com/weloveyolo/moniguard/ui/discover/DiscoverFragment.java
+++ app/src/main/java/com/weloveyolo/moniguard/ui/discover/DiscoverFragment.java
@@ -1,6 +1,8 @@
 package com.weloveyolo.moniguard.ui.discover;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,12 +11,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.weloveyolo.moniguard.MainActivity;
 import com.weloveyolo.moniguard.R;
 import com.weloveyolo.moniguard.activitys.AlbumActivity;
+import com.weloveyolo.moniguard.activitys.DiscoverFragmentActivity;
 import com.weloveyolo.moniguard.databinding.SmartAlbum1Binding;
 
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class DiscoverFragment extends Fragment {
 
@@ -23,6 +28,20 @@ public class DiscoverFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_discover, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ImageButton button1 = view.findViewById(R.id.smart_photo_album);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), AlbumActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 

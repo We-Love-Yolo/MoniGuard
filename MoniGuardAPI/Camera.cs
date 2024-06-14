fa36933ca7a2899cb7ff1b2d1adcb0b4193faae8
BitterOrange <ab12345678902014@outlook.com>
2024-06-14 22:21:45 +0800

    Update entity

--- MoniGuardAPI/Camera.cs
+++ MoniGuardAPI/Camera.cs
@@ -4,42 +4,57 @@ using System.Text.Json.Serialization;
 
 namespace MoniGuardAPI;
 
-public class Camera(int cameraId, string name, DateTime createdAt, int sceneId, string? description)
+/// <summary>
+/// 摄像头实体类。
+/// </summary>
+/// <param name="cameraId">摄像头ID。</param>
+/// <param name="name">摄像头名称。</param>
+/// <param name="createdAt">摄像头创建时间。</param>
+/// <param name="sceneId">摄像头所属场景的场景ID。</param>
+/// <param name="description">摄像头描述。</param>
+/// <param name="uniqueId">摄像头唯一ID。</param>
+public class Camera(int cameraId, string name, DateTime createdAt, int sceneId, string? description, Guid uniqueId)
 {
-
-    public Camera(string name, DateTime createdAt, int sceneId, string? description) : this(0, name, createdAt, sceneId, description)
-    {
-    }
+    /// <summary>
+    /// 使用指定的属性初始化一个新的摄像头实例。
+    /// </summary>
+    /// <inheritdoc cref="Camera(int, string, DateTime, int, string?, Guid)"/>
+    public Camera(string name, string? description, int sceneId, Guid uniqueId) : this(0, name, DateTime.Now, sceneId, description, uniqueId) { }
 
     /// <summary>
-    /// 使用名称和描述创建一个新的摄像头。该构造方法将自动设置创建时间为当前时间。
+    /// 摄像头ID。
     /// </summary>
-    /// <param name="name">摄像头名。</param>
-    /// <param name="description">摄像头描述。</param>
-    [JsonConstructor]
-    public Camera(string name, string? description) : this(name, DateTime.Now, 0, description)
-    {
-    }
-
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int CameraId { get; set; } = cameraId;
 
-    [Required]
-    [Length(2, 50)]
+    /// <summary>
+    /// 摄像头名称。
+    /// </summary>
+    [MinLength(2)]
+    [MaxLength(50)]
     public string Name { get; set; } = name;
 
-    [Required]
+    /// <summary>
+    /// 摄像头创建时间。
+    /// </summary>
     public DateTime CreatedAt { get; set; } = createdAt;
 
+    /// <summary>
+    /// 摄像头描述。
+    /// </summary>
     [MaxLength(255)]
     public string? Description { get; set; } = description;
 
-    [Required]
+    /// <summary>
+    /// 摄像头所属场景的场景ID。
+    /// </summary>
     public int SceneId { get; set; } = sceneId;
 
-    [Required] 
-    public string ConnectString { get; set; }
+    /// <summary>
+    /// 摄像头唯一ID。
+    /// </summary>
+    public Guid UniqueId { get; set; }
 }
 
 /// <summary>
@@ -47,6 +62,7 @@ public class Camera(int cameraId, string name, DateTime createdAt, int sceneId, 
 /// </summary>
 /// <param name="name">摄像头名称。</param>
 /// <param name="description">摄像头描述。</param>
+[method: JsonConstructor]
 public struct CameraDto(string name, string? description)
 {
     /// <summary>

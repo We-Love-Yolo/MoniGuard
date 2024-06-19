caf5bca8a673f5b25546d6cf8357df5220903b5b
Azureussimu <skymusky@163.com>
2024-06-20 01:20:39 +0800

    main至少能跑了

--- main.py
+++ main.py
@@ -1,72 +1,112 @@
-# import os
-# import authorization
-# from moni_guard_api.analysis_api import AnalysisApi
-# def showMenu():
-#     print("Menu:")
-#     print("1. Add Camera")
-#     print("2. List Cameras")
-#     print("3. Remove Cameras")
-#     print("5. Exit")
-#     num = input("Enter your choice: ")
-#     if isinstance(num, int):
-#         return num
-#     else:
-#         return -1
-#
-#
-# if __name__ == "__main__":
-#     camera_list = []
-#     print("Welcome to the camera management system!")
-#     print("Is time to log in")
-#     access_token = authorization.get_access_token()
-#     aapi = AnalysisApi("https://mgapi.bitterorange.cn/")
-#     aapi.set_access_token(access_token)
-#     os.system('clear')
-#     while True:
-#         choice = showMenu()
-#         if choice == 1:
-#             os.system('clear')
-#
-#         elif choice == 5:
-#             break
-#         else:
-#             print("Invalid choice")
-#             input("Press Enter to continue...")
-#             os.system('clear')
-#     print("Goodbye!")
-import numpy as np
 import os
+import asyncio
+import threading
 
-from moni_guard_api import analysis_api, analyze
 import authorization
+from moni_guard_api.analysis_api import AnalysisApi
+from moni_guard_api import analyze
 
 
-async def test():
-    # aapi = analysis_api.AnalysisApi("https://mgapi.bitterorange.cn")
+def showMenu():
+    print("Menu:")
+    print("1. Add Camera")
+    print("2. List Cameras")
+    print("3. Remove Cameras")
+    print("4. Exit")
+    return int(input("Enter your choice: "))
 
-    accessToken = await authorization.get_access_token()
-    aapi = analysis_api.AnalysisApi("http://192.168.239.101")
-    aapi.set_access_token(accessToken)
-    # with open("head.jpg", "rb") as f:
-    #     image = f.read()
-    #
-    # # guestId = await aapi.post_guest(10,"testName",False)
-    # # print(guestId)
-    # guestId = 6
-    # # with open("my_face_encoding_data.npy", "rb") as f:
-    # #     encodingData = f.read()
-    # # print(await aapi.post_guest_face_encoding(guestId, encodingData))
-    # # print(await aapi.post_face(guestId, image))
-    # print(await aapi.post_guest_to_photo(guestId, 5))
-    camera_index = 1
-    analyze1 = analyze.Analyze(f"camera{camera_index}_home", 10, 4, "rtsp://admin:WUsan53408@192.168.239.109", aapi)
-    await analyze1.analysis()
-    print('have start')
-    input("Press Enter to kill...")
-    analyze1.kill()
+
+
+def run_analyze(analyze11: analyze.Analyze):
+    # print('start')
+    asyncio.run(analyze11.analysis())
+
+
+def list_cameras(camera_list: list[analyze.Analyze]):
+    print("----------------------------------------------------")
+    print("Camera list:")
+    for i in range(len(camera_list)):
+        print(f"[running]{i} : {camera_list[i].RTSP_URL}")
+    print("----------------------------------------------------")
 
 
 if __name__ == "__main__":
-    import asyncio
-    asyncio.run(test())
+    camera_list = []
+    camera_thread_list = []
+    print("Welcome to the camera management system!")
+    print("Is time to log in")
+    access_token = asyncio.run(authorization.get_access_token())
+    aapi = AnalysisApi("https://mgapi.bitterorange.cn")
+    aapi.set_access_token(access_token)
+    # os.system('clear')
+    while True:
+        choice = showMenu()
+        if choice == 1:
+            # os.system('clear')
+            analyze1 = analyze.Analyze(f"camera{len(camera_list) + 1}_home", 10, 4,
+                                       "rtsp://admin:WUsan53408@192.168.239.109",
+                                       aapi)
+            camera_list.append(analyze1)
+            thread = threading.Thread(target=run_analyze, args=(analyze1,))
+            thread.start()
+            camera_thread_list.append(thread)
+            print("Camera added successfully!")
+        elif choice == 2:
+            # os.system('clear')
+            list_cameras(camera_list)
+        elif choice == 3:
+            while True:
+                num = input("Enter the camera number to remove: ")
+                num = int(num)
+                if num < len(camera_list):
+                    camera_list[num].kill()
+                    camera_list.pop(num)
+                    camera_thread_list[num].join()
+                    camera_thread_list.pop(num)
+                    print("Camera removed successfully!")
+                    break
+                else:
+                    print("Invalid camera number")
 
+        elif choice == 4:
+            break
+        else:
+            print("Invalid choice")
+            input("Press Enter to continue...")
+            os.system('clear')
+    print("Goodbye!")
+# import numpy as np
+# import os
+
+# from moni_guard_api import analysis_api, analyze
+# import authorization
+#
+#
+# async def test():
+#     # aapi = analysis_api.AnalysisApi("https://mgapi.bitterorange.cn")
+#
+#     accessToken = await authorization.get_access_token()
+#     aapi = analysis_api.AnalysisApi("http://192.168.239.101")
+#     aapi.set_access_token(accessToken)
+#     # with open("head.jpg", "rb") as f:
+#     #     image = f.read()
+#     #
+#     # # guestId = await aapi.post_guest(10,"testName",False)
+#     # # print(guestId)
+#     # guestId = 6
+#     # # with open("my_face_encoding_data.npy", "rb") as f:
+#     # #     encodingData = f.read()
+#     # # print(await aapi.post_guest_face_encoding(guestId, encodingData))
+#     # # print(await aapi.post_face(guestId, image))
+#     # print(await aapi.post_guest_to_photo(guestId, 5))
+#     camera_index = 1
+#     analyze1 = analyze.Analyze(f"camera{camera_index}_home", 10, 4, "rtsp://admin:WUsan53408@192.168.239.109", aapi)
+#     await analyze1.analysis()
+#     print('have start')
+#     input("Press Enter to kill...")
+#     analyze1.kill()
+#
+#
+# if __name__ == "__main__":
+#     import asyncio
+#     asyncio.run(test())
